5629c91329cf5c9e18a2ea1c0e6ffb9e8bff985b
Sachin Maurya <sachin22424@iiitd.ac.in>
2024-07-23 01:39:31 +0530

    Update index.js

--- server_backend/index.js
+++ server_backend/index.js
@@ -22,14 +22,21 @@ const corsOptions = {
         }
     },
     credentials: true, // Enable if you need to send cookies or HTTP authentication headers
+    optionsSuccessStatus: 200 // Some legacy browsers choke on 204
 };
 
 app.use(cors(corsOptions)); // Use CORS middleware before routes
 
+// Additional headers for debugging
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*"); // Allow all origins, you can change this to a specific one
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
 });
 
 app.use("/api/user", userRoute);
