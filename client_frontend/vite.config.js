eba925df803e76f4d6634390702fe981d64316ef
Sachin Maurya <sachin22424@iiitd.ac.in>
2024-07-22 23:05:36 +0530

    Handling Cors issue

--- client_frontend/vite.config.js
+++ client_frontend/vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'https://chit-chat-backend-97lm.onrender.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
   }
-})
+});
+
