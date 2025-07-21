210cd61cce4ab5e887106621e817bcc6a7ed4f04
Sachin Maurya <176058500+sachin123625@users.noreply.github.com>
2025-07-22 02:49:07 +0530

    Update index.js

--- server_backend/index.js
+++ server_backend/index.js
@@ -31,6 +31,16 @@ const corsOptions = {
 };
 
 app.use(express.json());
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header("Access-Control-Allow-Origin", origin);
+        res.header("Access-Control-Allow-Credentials", "true");
+        res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+        res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+    }
+    next();
+});
 app.use(cors(corsOptions));
 
 // API Routes
